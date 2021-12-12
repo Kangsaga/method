7006061dde4f8e455709c47124e59e12399fe64d
beoumsu kang <95010758+Kangsaga@users.noreply.github.com>
2021-12-13 08:56:25 +0900

    Add files via upload

---
+++ Exam05메소드연습2.java
@@ -0,0 +1,22 @@
+
+public class Exam05메소드연습2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 메소드의 리턴타입
+		// 1. int, String, double ....-> 리턴 타입 존재 O
+		// 2. void -> 리턴 타입 존재 X
+		
+		hello("빅데이터 반");
+		
+	}
+	public static void hello(String str) {
+		
+		System.out.println(str + " 반갑습니다~");
+		
+		
+		
+	}
+
+}
