ace8de5f86605e3e009088f716240299c896effd
beoumsu kang <95010758+Kangsaga@users.noreply.github.com>
2021-12-13 08:57:01 +0900

    Add files via upload

---
+++ Exam06메소드연습3.java
@@ -0,0 +1,27 @@
+
+public class Exam06메소드연습3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add(3,5);
+		sub(3,5);
+		mul(3,5);
+		div(3,5);
+	}
+	public static void add(int x1, int x2) {
+		int result = x1 + x2;
+		System.out.println(result);
+	}
+	public static void sub(int x1, int x2) {
+		int result = x1 - x2;
+		System.out.println(result);
+	}
+	public static void mul(int x1, int x2) {
+		int result = x1 * x2;
+		System.out.println(result);
+	}
+	public static void div(int x1, int x2) {
+		int result = x1 / x2;
+		System.out.println(result);
+	}
+}
